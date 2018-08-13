9ccee9e70963a64748d190ac1aa8c42318735061
陈明启 <30359281+Harry-qi@users.noreply.github.com>
2018-08-13 22:14:01 +0800

    Update Haeder.vue

--- src/components/Haeder.vue
+++ src/components/Haeder.vue
@@ -1,32 +1,60 @@
 <template>
-  <header>
-      <nav v-bind:style="background-color: #fff" v-on:mouseover:>
-          <ul class="navUl">
-              <li>菜单1</li>
-              <li>菜单1</li>
-              <li>菜单1</li>
-          </ul>
-      </nav>
-  </header>
+      <div class="header" @mouseover="changeBgW" @mouseleave="changeBgT" @scroll="changeBgW" :style="{background:color}">
+        <nav>
+            <ul class="navUl">
+                <li>菜单1</li>
+                <li>菜单1</li>
+                <li>菜单1</li>
+            </ul>
+        </nav>
+      </div>
 </template>
 
 <script>
 export default {
-  name: 'header',
+  name: 'headerBox',
   data(){
     return {
-    
+      color:"transparent"
     }
-  }
+  },
+  methods:{
+      changeBgW:function(e){
+              this.color = "#fff";
+              e = e || window.event;  
+                if (e.wheelDelta) {  //第一步：先判断浏览器IE，谷歌滑轮事件               
+                    if (e.wheelDelta > 0) { //当滑轮向上滚动时  
+                        console.log("滑轮向上滚动");  
+                    }  
+                    if (e.wheelDelta < 0) { //当滑轮向下滚动时  
+                        console.log("滑轮向下滚动");  
+                    }  
+                } else if (e.detail) {  //Firefox滑轮事件  
+                    if (e.detail> 0) { //当滑轮向上滚动时  
+                        console.log("滑轮向上滚动");  
+                    }  
+                    if (e.detail< 0) { //当滑轮向下滚动时  
+                        console.log("滑轮向下滚动");  
+                    }  
+                }  
+      },
+      changeBgT:function(){
+              this.color = "transparent"
+      }
+  },
+   created() {
+        window.addEventListener('scroll', this.changeBgW);
+    },
 }
 </script>
 
 <style>
-header{
-    position: relative;
+.header{
+    position: fixed;
     top: 0%;
     z-index: 999;
-    
+    width: 100%;
+    height: 60px;
 }
 nav{
     height: 60px;
